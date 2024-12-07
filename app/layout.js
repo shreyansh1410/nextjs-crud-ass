8e53fd851b0b840e0cb4888124bd37893a9a5c33
Shreyansh Shukla <shreyansh.14010@gmail.com>
2024-12-07 11:25:25 +0530

    minor changes

--- app/layout.js
+++ app/layout.js
@@ -13,7 +13,7 @@ const geistMono = localFont({
 });
 
 export const metadata = {
-  title: "Task Management App",
+  title: "Task Management App by Shreyansh",
   description: "CRUD Task Management Application",
 };
 
